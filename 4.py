e53001ae5f3e994755ec3963c9b7c7ccf1f1878b
Tapo4ek <akina.448@mail.ru>
2023-06-23 14:54:15 +0400

    first commit2

---
+++ 4.py
@@ -0,0 +1,19 @@
+import os
+
+current_path = os.path.abspath(__file__)
+
+parent_path = os.path.join(current_path, '..')
+
+print(current_path)
+print(parent_path)
+
+def get_all_files(path):
+    for i_name in os.listdir(path):
+        new_path = os.path.join(path, i_name)
+        if os.path.isdir(new_path):
+            print('Папка', i_name)
+            get_all_files(new_path)
+        №else:
+            print('-', i_name)
+
+get_all_files(parent_path)
---
+++ 5.py
@@ -0,0 +1,8 @@
+#s = 'aaabca'
+#sets = set(s)
+#for i in sets:
+#    print(i, ':' , s.count(i))
+
+
+
+
